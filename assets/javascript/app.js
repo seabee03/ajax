60a3f5ecc234152b317ed160fb98b9b4a557b332
Seabee03 <34292383+seabee03@users.noreply.github.com>
2019-06-10 23:06:42 -0500

    added json file, could not debug rating or images to get it to show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,42 @@
 // Array of players
 var players = ["Kawhi Leonard", "Tony Parker", "Kobe Bryant", "Lebron James", "Shaq"];
 
+// 
 function displayPlayerInfo() {
-    var player = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=pM7BRdBRDkZ7zM5AjIWd4jim5DaQR31h&limit=5";
 
+    // where api is
+    var player = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=pM7BRdBRDkZ7zM5AjIWd4jim5DaQR31h&limit=1";
+
+    // Ajax to get information
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+
+        // Players to go here
         var playerDiv = $("<div class='player'>");
-        var rating = response.Rated;
-        var pOne = $("<p>").text("Rating: " + rating);
+
+        // adding the rating
+        // I AM STILL HAVING TROUBLE WITH THIS
+        var rated = response.rating;
+        var pOne = $("<p>").text("Rating: " + rated);
         playerDiv.append(pOne);
-        var imgURL = $("<img>").attr("src", imgURL);
+
+        // adding the image
+        // I AM HAVING TROUBLE WITH THIS AS WELL
+        var imgURL = response.original_still;
+        var image = $("<img>").attr("src", imgURL);
         playerDiv.append(image);
         $("#players-view").prepend(playerDiv);
     });
 }
 
+// clears prior players before adding new players
 function renderButtons() {
     $("#buttons-view").empty();
+
+    // loop for players
     for (var i = 0; i < players.length; i++) {
         var a = $("<button>");
         a.addClass("player-btn");
@@ -30,12 +46,18 @@ function renderButtons() {
     }
 }
 
+// adding a button click event
 $("#add-player").on("click", function(event) {
+
+    // prevents browser from refreshing
     event.preventDefault();
     var player = $("#player-input").val().trim();
+
+    // adds players to the array
     players.push(player);
     renderButtons();
 });
 
+// "listens" for this event 
 $(document).on("click", ".player-btn", displayPlayerInfo);
 renderButtons();
