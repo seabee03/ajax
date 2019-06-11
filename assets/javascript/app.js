e4336aa52db52a7f495be49284d7ce567d9b457c
Seabee03 <34292383+seabee03@users.noreply.github.com>
2019-06-10 21:56:45 -0500

    had trouble using the format the tutor showed me, redid html layout and restarted on JS. added array of players, and ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,20 @@
-// Topic is sports players
-var player = [
-    "Kawhi Leonard", "Lebron James", "Payton Manning", "Gerard Butler", "Cam Newton"
-];
-generateButtons();
+// Array of players
+var players = ["Kawhi Leonard", "Tony Parker", "Kobe Bryant", "Lebron James", "Shaq"];
 
-// This will generate the buttons for the players listed already in the topic
-function generateButtons() {
-    $("#buttons-holder").empty();
-    console.log("inside the generateButtons function")
-    for (var i = 0; i < player.length; i++) {
-        var a = $("<button>");
-        a.addClass("classToAdd");
-        a.attr("data-type", player[i]);
-        a.text(player[i]);
-        console.log(a);
-        $("#buttons-holder").append(a);
-    }
-}
+function displayPlayerInfo() {
+    var player = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player "&api_key=pM7BRdBRDkZ7zM5AjIWd4jim5DaQR31h&limit=5";
 
-// adding the event "click"
-$("#add").on("click", function(event) {
-    // Prevents browser from refreshing and prevents repeating buttons
-    event.preventDefault();
-    // allows for user to add a new player
-    var newPlayer = $("#player-input").val();
-    console.log(newPlayer);
-    // pushes the information to the generateButtons function
-    player.push(newPlayer);
-    generateButtons();
-
-})
-
-// creating function for ajax to "GET"
-$(".classToAdd").on("click", function() {
-    $("#results").empty();
-    var queryURL = $(this).attr("data-type");
     $.ajax({
-        url: "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=pM7BRdBRDkZ7zM5AjIWd4jim5DaQR31h&limit=5",
+        url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        var results = response.data;
-    })
-})
+    }).them(function(response) {
+        var playerDiv = $("<div class='player'>");
+        var rating = response.Rated;
+        var pOne = $("<p>").text("Rating: " + rating);
+        playerDiv.append(pOne);
+        var imgURL = $("<img>").attr("src", imgURL);
+        playerDiv.append(image);
+        $("#players-view").prepend(playerDiv);
+    });
+}
